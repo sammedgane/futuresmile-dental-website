2e8da0d1026d5baca5039135f3054d64ac376fc5
sammedgane <sammedgane143@gmail.com>
2026-03-06 11:13:35 +0530

    Update smileScanner.js

--- smileScanner.js
+++ smileScanner.js
@@ -21,3 +21,30 @@ Possible improvements:
 },2000)
 
 })
+
+function openScanner() {
+
+const input = document.createElement("input");
+
+input.type = "file";
+input.accept = "image/*";
+
+input.onchange = function () {
+
+const file = input.files[0];
+
+alert("Analyzing smile...");
+
+setTimeout(() => {
+
+alert(
+"Analysis Result:\n\n• Teeth whitening recommended\n• Slight alignment issue\n• Consider cosmetic dentistry"
+);
+
+},2000);
+
+};
+
+input.click();
+
+}
