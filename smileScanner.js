3f94df5798b7242017f62aa3140a766a47d6a5f3
sammedgane <sammedgane143@gmail.com>
2026-03-06 11:15:23 +0530

    Update smileScanner.js

--- smileScanner.js
+++ smileScanner.js
@@ -1,27 +1,3 @@
-const upload=document.getElementById("upload")
-
-const result=document.getElementById("result")
-
-upload.addEventListener("change",()=>{
-
-result.innerHTML="Analyzing smile..."
-
-setTimeout(()=>{
-
-result.innerHTML=
-
-`
-Possible improvements:
-
-• Teeth whitening recommended  
-• Slight alignment correction  
-• Cosmetic smile enhancement
-`
-
-},2000)
-
-})
-
 function openScanner() {
 
 const input = document.createElement("input");
